0807a497896143054e6f4290216398302faea957
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-24 19:19:31 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,22 +4,10 @@ require 'json'
 require 'pry'
 
 class GetRequester
-
-  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-
-  def get_response_body
-    uri = URI.parse(URL)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def parse_json
-# we use the JSON library to parse the API response into nicely formatted JSON
-  programs = JSON.parse(self.get_response_body)
-  programs.collect do |program|
-    program["agency"]
-  end
+ def initialize(url)
+  @url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 end
+
 
 programs = GetRequester.new.get_response_body
 puts programs
