2ac2aec109c8936c168bced7cf833d8b3d2f9454
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-24 18:52:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -13,12 +13,3 @@ class GetRequester
     response.body
   end
 
-  def parse_json
-# we use the JSON library to parse the API response into nicely formatted JSON
-  programs = JSON.parse(self.get_programs)
-  programs.collect do |program|
-    program["agency"]
-  end
-end
-programs = GetPrograms.new.parse_json
-puts programs
