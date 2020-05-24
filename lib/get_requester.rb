ae20ce56a8401ccd30946b6e44ccef94c94d98bf
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-24 20:10:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'pry'
 
 class GetRequester
    def initialize(url)
-URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+     @url = url    
    end
 
    def get_response_body
