e1ca179e796cb779dc9dd63313c2ee2825cc4814
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-24 18:41:44 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,5 +2,7 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 require 'pry'
-get_requester = Get_requester.new('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
-get_requester.parse_json
+
+class GetRequester
+
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
